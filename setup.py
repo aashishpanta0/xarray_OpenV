a9857a6191766c0547c33ebb9bee054ff7cd64d5
aashishpanta0 <aashishpanta0@gmail.com>
2022-01-14 00:55:32 -0700

    adding local files

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     entry_points={
+        # set this to the location of core xarray backend
         "xarray.backends": ["openvis=xarray_files.backend:NewBackendPoint"]
         }
 
