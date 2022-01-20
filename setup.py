28e34da6ce3f30581bfb8d34cef78e22df43d8b8
aashishpanta0 <aashishpanta0@gmail.com>
2022-01-20 00:05:16 -0700

    adding lazyloading framework

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     entry_points={
         # set this to the location of core xarray backend
-        "xarray.backends": ["openvis=xarray_files.backend:NewBackendPoint"]
+        "xarray.backends": ["openvis_engine=xarray_files.backend:OpenVisusEntryPoint"]
         }
 
 )
--- xarray_files/backend.py
+++ xarray_files/backend.py
@@ -1,10 +1,13 @@
 import os.path
 import numpy as np
 import xarray as xr
-from xarray.backends import BackendEntrypoint
-from xarray.backends import BackendArray
+from xarray.backends.common import (
+    BACKEND_ENTRYPOINTS,
+    BackendArray,
+    BackendEntrypoint,
+)
 from xarray.core import indexing
-
+from xarray.core.indexing import ExplicitIndexer, LazilyIndexedArray
 
 
 # all extensions that can be loaded (must be changed later)
@@ -24,7 +27,7 @@ CAN_OPEN_EXTS = {
 }
 
 
-class NewBackendPoint(BackendEntrypoint):
+class OpenVisusEntryPoint(BackendEntrypoint):
  
 
     def open_dataset(
@@ -45,6 +48,21 @@ class NewBackendPoint(BackendEntrypoint):
         open_kwargs=None,
     ):
         open_dataset_parameters = ["filename_or_obj", "drop_variables"]
+        backend_array=OpenVisusBackendArray()
+        """
+        
+        """
+        data = LazilyIndexedArray(backend_array)
+        """ can select the variables and dimensions by updating the xr.variable(data)
+        to xr.variable(dims,attrs,data)
+        """
+        vars= xr.Variable( data)
+        vars.set_close(self.close_method)
+        return vars
+    
+    def close_method(self):
+        return
+    
       
 
     def guess_can_open(self, filename_or_obj):  # pylint: disable=arguments-renamed
@@ -54,7 +72,26 @@ class NewBackendPoint(BackendEntrypoint):
             return False
         return ext[1:].lower() in CAN_OPEN_EXTS
 
+# subclass implementation of BackendArray
+class OpenVisusBackendArray(BackendArray):
+    def __init__(
+        self,
+        shape,
+        dtype,
+        lock,
+        # other backend specific keyword arguments
+    ):
+        self.shape = shape
+        self.dtype = lock
+        self.lock = dtype
 
-
-#indexing here
+    def __getitem__(
+        self, key: ExplicitIndexer
+    ) -> np.typing.ArrayLike:
+        return indexing.explicit_indexing_adapter(
+            key,
+            self.shape,
+            indexing.IndexingSupport.BASIC,
+            self._raw_indexing_method,
+        )
 
