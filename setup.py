c179937973a0338c51e3130a24a3d5e508726d33
aashishpanta0 <aashishpanta0@gmail.com>
2022-01-20 00:07:43 -0700

    updating entrypoing for lazyloading

--- setup.py
+++ setup.py
@@ -3,11 +3,7 @@ from setuptools import find_packages, setup
 setup(
     entry_points={
         # set this to the location of core xarray backend
-<<<<<<< HEAD
-        "xarray.backends": ["openvis_engine=xarray_files.backend:OpenVisusEntryPoint"]
-=======
-        "xarray.backends": ["openvis=xarray_files.xarray_plugin:NewBackendPoint"]
->>>>>>> 7be4d8f76d7547cbe37a11dec64cf76f2fe4fa5c
+        "xarray.backends": ["openvis_engine=xarray_files.xarray_plugin:OpenVisusEntryPoint"]
         }
 
 )
