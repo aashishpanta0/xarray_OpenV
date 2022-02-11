97ca0fcd89b817453846bacf7cc9fe5133182636
aashishpanta0 <aashishpanta0@gmail.com>
2022-02-10 18:26:50 -0700

    adding multi-time and multiresolution part

--- xarray_files/xarray_backend.py
+++ xarray_files/xarray_backend.py
@@ -1,8 +1,7 @@
-from xarray.backends.common import BACKEND_ENTRYPOINTS
 import xarray as xr
 import numpy  as np
 import pandas as pd
-import os
+
 # !pip install OpenVisusNoGui
 import OpenVisus as ov
 
@@ -13,42 +12,90 @@ import OpenVisus as ov
 class OpenVisusBackendArray(xr.backends.common.BackendArray):
 
     # constructor
-    def __init__(self,db, shape, dtype, ncomponents):
+    def __init__(self,db, shape, dtype, timesteps,resolution,ncomponents):
         self.db    = db
         self.shape = shape
+        print(self.shape)
         self.dtype = dtype
         self.ncomponents=ncomponents
         self.pdim=db.getPointDim()
+        self.timesteps=timesteps
+        self.resolution=resolution
+        
 
     # __getitem__
     def __getitem__(self, key: xr.core.indexing.ExplicitIndexer) -> np.typing.ArrayLike:
-        return xr.core.indexing.explicit_indexing_adapter(key,self.shape,xr.core.indexing.IndexingSupport.BASIC,self._raw_indexing_method)
+        return xr.core.indexing.explicit_indexing_adapter(key,self.shape,
+                                                          xr.core.indexing.IndexingSupport.BASIC,
+                                                          self._raw_indexing_method)
 
     # _getKeyRange
     def _getKeyRange(self, value):
         A = value.start if isinstance(value, slice) else value    ; A = 0             if A is None else A
         B = value.stop  if isinstance(value, slice) else value + 1; B = self.shape[1] if B is None else B
         return (A,B)
-
     # __readSamples
     def _raw_indexing_method(self, key: tuple) -> np.typing.ArrayLike:
         print("_raw_indexing_method","key",key)
 
         if self.pdim==2:
-            y1,y2=self._getKeyRange(key[0])
-            x1,x2=self._getKeyRange(key[1])
-            data=self.db.read(logic_box=[(x1,y1),(x2,y2)])
+            
+            y1,y2=self._getKeyRange(key[2])
+            x1,x2=self._getKeyRange(key[3])
+            
+            if type(self.resolution)==int:
+
+                res2=self.resolution
+            else:
+                res1,res2=self._getKeyRange(key[1])
+            
+            
+            if type(self.timesteps)==int:
+                res2-=1
+
+                data=self.db.read(time=self.timesteps,max_resolution=res2, logic_box=[(x1,y1),(x2,y2)])
+                
+            else:
+                t1,t2=self._getKeyRange(key[0])
+                if type(t2) ==int and type(res2)==int:
+                    res2-=1
+                    t2-=1
+
+                    data=self.db.read(time=t2,max_resolution=res2,logic_box=[(x1,y1),(x2,y2)])
+                else:
+                    data=self.db.read(logic_box=[(x1,y1),(x2,y2)])   
+                      
+            
 
         elif self.pdim==3:
-            z1,z2=self._getKeyRange(key[0])
-            y1,y2=self._getKeyRange(key[1])
-            x1,x2=self._getKeyRange(key[2])
-            data=self.db.read(logic_box=[(x1,y1,z1),(x2,y2,z2)])
+            if type(self.resolution)==int:
+                r2=self.resolution
+            else:
+                r1,r2=self._getKeyRange(key[1])
+            
+
+            z1,z2=self._getKeyRange(key[2])
+            y1,y2=self._getKeyRange(key[3])
+            x1,x2=self._getKeyRange(key[4])
+
+            if type(self.timesteps)==int:
+                res2-=1
+                data=self.db.read(time=self.timesteps,max_resolution=r2, logic_box=[(x1,y1,z1),(x2,y2,z2)])
+                
+            else:
+                t1,t2=self._getKeyRange(key[0])
+                if type(t2) == int and type(r2)==int:
+                    res2-=1
+                    t2-=1
+                    data=self.db.read(time=t2, max_resolution=r2,logic_box=[(x1,y1,z1),(x2,y2,z2)])
+                else:
+                    data=self.db.read(logic_box=[(x1,y1,z1),(x2,y2,z2)]) 
+                
 
         else:
             raise Exception("problem")
 
-        # last key element is the channel
+#         # last key element is the channel
         if self.ncomponents>1:
             c1,c2=self._getKeyRange(key[-1])
 
@@ -74,23 +121,22 @@ class OpenVisusBackendEntrypoint(xr.backends.common.BackendEntrypoint):
     open_dataset_parameters = ["filename_or_obj", "drop_variables", "resolution", "timesteps"]
     
     # open_dataset
-    def open_dataset(self,filename_or_obj,*, resolution=None, timesteps=None, drop_variables=None):
+    def open_dataset(self,filename_or_obj,*, resolution=None, timesteps=None,drop_variables=None):
 
-        # TODO for now only full resoluton
+
         self.resolution=resolution
 
-        # TODO for now only default timestep
-        self.timesteps=timesteps
 
         data_vars={}
 
         db=ov.LoadDataset(filename_or_obj)
-
+#         print(db.getMaxResolution())
+        self.timesteps=timesteps
         dim=db.getPointDim()
         dims=db.getLogicSize()
+        if self.timesteps==None: 
+            self.timesteps=[int(it) for it in db.getTimesteps().asVector()]
 
-#         print(db.getDatasetBody().toString())
-#         print("dim",dim, "dims",dims)
 
         # convert OpenVisus fields into xarray variables
         for fieldname in db.getFields():
@@ -117,16 +163,33 @@ class OpenVisusBackendEntrypoint(xr.backends.common.BackendEntrypoint):
 
             shape=list(reversed(dims))
             if ncomponents>1:
+                
+                labels.insert(0,"resolution")
+                labels.insert(0,"time")
                 labels.append("channel")
                 shape.append(ncomponents)
+                if type(self.resolution) == int:
+                    shape.insert(0,self.resolution)  
+                else:
+                    shape.insert(0,db.getMaxResolution())
+                if type(self.timesteps) == int:
+                    shape.insert(0,self.timesteps)  
+                else:
+                    shape.insert(0,len(self.timesteps))
+                    
+
 
             data_vars[fieldname]=xr.Variable(
                 labels, 
-                xr.core.indexing.LazilyIndexedArray(OpenVisusBackendArray(db=db, shape=shape,dtype=dtype, ncomponents=ncomponents)), 
+                xr.core.indexing.LazilyIndexedArray(OpenVisusBackendArray(db=db, shape=shape,dtype=dtype,
+                                                                          timesteps=self.timesteps,
+                                                                          resolution=self.resolution,
+                                                                          ncomponents=ncomponents)), 
                 attrs={} # no attributes
             )
 
-            print("Adding field",fieldname,"shape",shape,"dtype",dtype,"labels",labels)
+            print("Adding field",fieldname,"shape",shape,"dtype",dtype,"labels",labels,"timesteps",self.timesteps,
+                 "Max Resolution", db.getMaxResolution())
 
         ds = xr.Dataset(data_vars=data_vars)
         ds.set_close(self.close_method)
@@ -164,5 +227,4 @@ class OpenVisusBackendEntrypoint(xr.backends.common.BackendEntrypoint):
         except TypeError:
             return False
         return ext.lower()==".idx"
-BACKEND_ENTRYPOINTS["openvisus"]=OpenVisusBackendEntrypoint
 
